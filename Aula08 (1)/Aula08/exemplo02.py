86b7d7a1d1a1f3ed2c089aded891fae505eced55
AdalAMN <141356540+AdalAMN@users.noreply.github.com>
2023-08-10 15:45:37 -0400

    Add files via upload

---
+++ Aula08 (1)/Aula08/exemplo02.py
@@ -0,0 +1,9 @@
+#Funções com retorno
+def some(v1,v2):
+    total = v1+v2
+    print(total)
+
+
+soma(10,20)
+#mostrar o dobro do resultado da sunção
+print(total)
---
+++ Aula08 (1)/Aula08/main.py
@@ -0,0 +1,15 @@
+import Exemplo01
+from exemplo02 import *
+from Exemplo04 import *
+
+
+
+
+if __name__ == '__main__':
+    Exemplo01.mensagem()
+    Exemplo01.calcular_dobro(5)
+    Exemplo01.calcular_metade(58)
+    print(media_tres(10,8,7))
+    print(maior_valor(5,6))
+    print(soma_coxinha(10,6,9,8))
+
